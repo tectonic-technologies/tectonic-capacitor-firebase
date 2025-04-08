057515466a0858f57076eecc7fafc696566f10b7
Hitesh Kumar <hitesh@tectonic.so>
2025-04-08 11:14:03 +0530

    v1.2.0

--- packages/analytics/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/analytics/GetSessionIdCallback.java
+++
@@ -1,8 +0,0 @@
-package io.capawesome.capacitorjs.plugins.firebase.analytics;
-
-import androidx.annotation.Nullable;
-
-public interface GetSessionIdCallback {
-    void success(@Nullable Long appSessionId);
-    void error(String message);
-}
